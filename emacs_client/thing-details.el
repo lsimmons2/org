86899dce82703ade2825e0a3d6a659a5d3b0cc8e
Leo Simmons <leooscar.simmons@gmail.com>
2025-01-10 19:17:03 -0500

    go to tag details from tag "pill" in thing details

--- emacs_client/thing-details.el
+++ emacs_client/thing-details.el
@@ -4,8 +4,18 @@
 (require 'domain)
 (require 'utils)
 
+(defun thing-details-visit-tag ()
+  "Visit the tag under the cursor."
+  (interactive)
+  (let ((tag-id (get-text-property (point) 'tag-id))) ;; Retrieve `tag-id` at point
+    (if tag-id
+        (view-tag-details tag-id)
+      (message "No tag under the cursor.")))) ;; Fallback message
+
 (define-derived-mode thing-details-mode special-mode "Thing-Details"
-  "Major mode for viewing details of a single thing.")
+  "Major mode for viewing details of a single thing."
+  (define-key thing-details-mode-map (kbd "RET") #'thing-details-visit-tag))
+
 
 (defun render-thing-details (thing)
   "Render the details of THING and its associated TAGS."
@@ -15,9 +25,16 @@
     (insert (format "%s\n%s\n\n" (format-header "Description:") (alist-get 'text thing)))
     (insert (format "%s\n" (format-header "Tags:")))
     (dolist (tag (append (alist-get 'tags thing) nil))
-      (insert (format "[%s] " (alist-get 'name tag))))
-    )
+      (let ((tag-name (alist-get 'name tag))
+            (tag-id (alist-get 'id tag)))
+        ;; Add text properties to associate the tag ID with the text
+        (insert-text-button
+         (format "[%s] " tag-name)
+         'action (lambda (_) (view-tag-details tag-id)) ;; Clickable action
+         'follow-link t
+         'tag-id tag-id)))) ;; Add `tag-id` as a text property
   )
+)
 
 (defun view-thing-details (thing-id)
   "Display the details of a thing by THING-ID."
